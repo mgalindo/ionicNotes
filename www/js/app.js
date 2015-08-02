9590b570cc77077d03b0b46d4cd49077aab86208
Miguel Galindo <mgalindob@gmail.com>
2015-08-02 13:32:12 -0500

    Step 2, Edit form

--- www/js/app.js
+++ www/js/app.js
@@ -1,47 +1,78 @@
-// Ionic Starter App
+( function() {
+  var app = angular.module('starter', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic']);
+  app.config(function($stateProvider, $urlRouterProvider) {
 
-app.controller('ListCtrl', function($scope) {
+    $stateProvider.state('list', {
+      url: '/list',
+      templateUrl: 'templates/list.html'
+    });
 
-  $scope.notes = [
-    { title: 'First Note',
+    $stateProvider.state('edit', {
+      url: '/edit/:noteId',
+      templateUrl: 'templates/edit.html'
+    });
+
+    $urlRouterProvider.otherwise('/list')
+  });
+
+   var notes = [
+    {
+      id: '1',
+      title: 'First Note',
       description: 'First Description'
     },
-    { title: 'Second Note',
+    {
+      id: '2',
+      title: 'Second Note',
       description: 'Second Description'
     }
+  ];
 
-  ]
-});
-
-app.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider.state('list', {
-    url: '/list',
-    templateUrl: 'templates/list.html'
-  });
-
-  $stateProvider.state('edit', {
-    url: '/edit',
-    templateUrl: 'templates/edit.html'
-  });
-
-  $urlRouterProvider.otherwise('/list')
-});
-
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  function getNote(noteId) {
+    for (var i=0; i < notes.length; i++) {
+      if (notes[i].id === noteId) {
+        return notes[i];
+      }
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    return undefined;
+  }
+
+  function updateNote(note) {
+    for (var i=0; i < notes.length; i++) {
+      if (notes[i].id === note.id) {
+        notes[i] = note;
+        return;
+      }
     }
+  }
+
+  app.controller('ListCtrl', function($scope) {
+    $scope.notes = notes;
   });
-});
+
+
+  app.controller('EditCtrl', function($scope, $state) {
+
+    $scope.note = angular.copy(getNote($state.params.noteId));
+
+    $scope.save = function() {
+      updateNote($scope.note);
+      $state.go('list');
+    }
+
+  });
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  });
+}() );
