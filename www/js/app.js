856be94f0317c8b5c17ec37cb54c7180f9fef1cf
Miguel Galindo <mgalindob@gmail.com>
2015-08-02 12:49:10 -0500

    Step 1, List, Navigation, Edit shell

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,37 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.controller('ListCtrl', function($scope) {
+
+  $scope.notes = [
+    { title: 'First Note',
+      description: 'First Description'
+    },
+    { title: 'Second Note',
+      description: 'Second Description'
+    }
+
+  ]
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+
+  $stateProvider.state('edit', {
+    url: '/edit',
+    templateUrl: 'templates/edit.html'
+  });
+
+  $urlRouterProvider.otherwise('/list')
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +44,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
